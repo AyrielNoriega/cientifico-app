be833173a6d8fd2f23328637c695c0f3ab8aed41
AyrielNoriega <anoriega2310@gmail.com>
2023-04-10 17:56:54 -0500

    first page

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+
+const Home = () => {
+    const view = `
+        <div class="characters">
+            <article class="character-item">
+                <a href="#/1/"></a>
+                <img src="img" alt="img"/>
+                <h2>Name</h2>
+            </article>
+        </div>
+    
+    `;
+
+    return view;
+}
+
+
+export default Home;
