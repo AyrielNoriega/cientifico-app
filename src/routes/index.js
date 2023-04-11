890b179368eb3a567a0aac8b3a13b581f0adc94a
AyrielNoriega <anoriega2310@gmail.com>
2023-04-10 22:11:57 -0500

    Conectar las rutas con los templates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import {Home, Character} from "../pages"
 import {Header} from "../templates";
 import getHash from "../utils/getHash";
+import resolveRoutes from "../utils/resolveRoutes";
 
 const routes = {
     '/': Home,
